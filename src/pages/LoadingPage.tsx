cc3a2b86b36cab117b30db0dc6053d100a87c10d
KadsSar <tn24yv@brocku.ca>
2026-03-24 18:09:24 -0400

    refactor: remove unnecessary React import from LoadingPage.tsx

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Bot, Building2 } from 'lucide-react';
 
